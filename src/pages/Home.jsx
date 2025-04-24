4a2db71d3d2e05a3ae69ed76e1fc901cedc3b9ff
RoseofKendy <162413453+RoseofKendy@users.noreply.github.com>
2025-04-24 22:20:20 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect, useRef, useCallback } from 'react';
 import { useBookSearch } from '../hooks/useBookSearch';
 import { BookOfTheDay } from '../components/BookOfTheDay';
